641b07258c4101e1a0dc7ae30c27ba7afd993afb
Renat Baryshnikov <renat-baryshnikov@mail.ru>
2023-08-02 15:19:26 +0300

    add task one

---
+++ src/practicalTask1/Task4.java
@@ -0,0 +1,12 @@
+package practicalTask1;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int age = 24;
+
+        while(age <= 100){
+            System.out.println("Мой возраст " + age);
+            age += 5;
+        }
+    }
+}
